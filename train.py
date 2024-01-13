415f06d5a5d7f8fce3a98ac7ecbadee012f05a87
Aleksandr Kazakov <alexkazakov@fb.com>
2024-01-13 11:31:17 +0000

    Add tensor

--- train.py
+++ train.py
@@ -1,15 +1,17 @@
 #!env python3
 
+import torch
+
 with open("input.txt", "r", encoding="utf-8") as f:
   text = f.read()
 
-chars = sorted(list(set(text)))
-vocab_size = len(chars)
+vocab = sorted(list(set(text)))
+stoi = { ch:i for i,ch in enumerate(vocab) }
+itos = { i:ch for i,ch in enumerate(vocab) }
 
-stoi = { ch:i for i,ch in enumerate(chars) }
-itos = { i:ch for i,ch in enumerate(chars) }
 encode = lambda s: [stoi[c] for c in s]
 decode = lambda l: "".join([itos[i] for i in l])
 
-print(encode("hello there"))
-print(decode(encode("hello there")))
+data = torch.tensor(encode(text), dtype=torch.long)
+print(data.shape, data.dtype)
+print(data[:1000])
